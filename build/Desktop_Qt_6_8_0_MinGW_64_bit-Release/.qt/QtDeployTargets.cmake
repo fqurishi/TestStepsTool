78173e6b58438a921f4aae9d287baacffc4ce9bb
fqurishi <faislqurishi@gmail.com>
2024-11-14 12:52:11 -0800

    update to ignore build and debug folders

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TestStepsTool_FILE D:/QTProjects/TestStepsTool/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/TestStepsTool.exe)
-set(__QT_DEPLOY_TARGET_TestStepsTool_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TestStepsTool_RUNTIME_DLLS S:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;S:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;S:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;S:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
